ca9065aa560437fe72ebbfb98039837e0a2d3af6
Stéphane Nawrat <stephane.nawrat@laplateforme.io>
2026-04-13 12:10:19 +0200

    feat: encapsulate game logic into a function triggered by button click

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let prompt = "Choissez un nombre : ";
+// 1. On cible le bouton
+const startButton = document.querySelector("button");
+
+// 2. On définit ce qui doit se passer (La boîte)
+function play() {
+  // Ton code actuel qui se lançait tout seul va ici
+  let userGuess = prompt("Choisissez un nombre :");
+  console.log("L'utilisateur a choisi :", userGuess);
+}
+
+// 3. On lie les deux : "N'exécute 'play' QUE quand on clique sur 'startButton'"
+startButton.addEventListener("click", play);
